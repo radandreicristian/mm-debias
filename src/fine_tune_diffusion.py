b06c5e2e050a5a93e32881bacbc90841e4c9a27d
Vicentiu <cojocaruvicentiu25@yahoo.com>
2024-08-18 13:57:17 +0300

    use shell script for fine-tuning

--- src/fine_tune_diffusion.py
+++
@@ -1,5 +0,0 @@
-from tti_fine_tune.test_text_to_image import TextToImage
-import unittest
-
-if __name__ == "__main__":
-    unittest.main(argv=[''], defaultTest='TextToImage.test_text_to_image', exit = False)
